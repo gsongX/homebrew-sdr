fe9ddfd8efe1de24a5562d0c2a6953285be71448
gsong <gsong2014@foxmail.com>
2019-10-26 12:00:51 +0700

    update gr-limesdr

--- Formula/gr-limesdr.rb
+++ Formula/gr-limesdr.rb
@@ -3,7 +3,7 @@ class GrLimesdr < Formula
   homepage "https://wiki.myriadrf.org/Gr-limesdr_Plugin_for_GNURadio"
   url "https://github.com/myriadrf/gr-limesdr/archive/v2.0.0.tar.gz"
   sha256 "466dd049daf32531ed717b6c2daf07881cad27b7ce037c53cb947e5dadd948b8"
-  head "https://github.com/eblot/gr-limesdr.git", :branch => "python3"
+  head "https://github.com/myriadrf/gr-limesdr.git", :branch => "gr-3.8"
 
   depends_on "cmake" => :build
   depends_on "ninja" => :build
@@ -11,10 +11,16 @@ class GrLimesdr < Formula
   depends_on "gmp"
   depends_on "swig"
   depends_on "gsong2014/sdr/limesuite"
-  depends_on "gsong2014/sdr/gnuradio"
+  depends_on "gsong2014/sdr/gnuradio@3.8"
+
+   # fix
+  patch do
+    url "https://raw.githubusercontent.com/gsong2014/homebrew-sdr/master/patch/gr-limesdr-python3+gnuradio3.8.patch"
+    sha256 "4aa968825c60d152c7f2a07125aa0aba0adbe156eb5420c9afca6582eb6f01ae"
+  end
 
   def install
-    python = Formulary.factory 'python'
+    python = Formulary.factory 'python3'
 
     args = %W[
       -DPYTHON_EXECUTABLE=#{python.bin}/python3
